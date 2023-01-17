d2511573d2eca303a6dd5f92ac5893bf7fa137a7
AaTekle <108105300+AaTekle@users.noreply.github.com>
2023-01-16 19:00:45 -0500

    Add files via upload

---
+++ First Python Program.py
@@ -0,0 +1,8 @@
+name = 'Aaron'
+password = 'Aaron1'
+if name == 'Aaron':
+    print('Hello Aaron!')
+    if password == 'Aaron1':
+        print('Access Granted, Welcome!')
+else:
+    print('Access Denied!')
